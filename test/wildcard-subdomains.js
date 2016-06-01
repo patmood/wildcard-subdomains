bf4cfc35b6f05b436113563defd32903431edca2
patmood <patrick.n.moody@gmail.com>
2016-06-01 09:11:14 -0700

    more tests and fix bug with including www

--- test/wildcard-subdomains.js
+++ test/wildcard-subdomains.js
@@ -1,20 +1,69 @@
 var test = require('tape')
 var request = require('supertest')
 var express = require('express')
-var wildcardSubdomains = require('../')({ domain: 'test.com' })
+var wildcardSubdomains = require('../')
 
-var app = express()
-app.use(wildcardSubdomains)
-app.get('/', function (req, res){
-  res.end(req.hostname)
-})
+test('default subdomain namespace', function (t) {
+  var app = express()
+  app.use(wildcardSubdomains({
+    domain: 'test.com',
+  }))
 
-test('request test', function (t) {
+  app.get('/sub/cat', function (req, res) {
+    res.end(req.hostname)
+  })
+
   t.plan(1)
   request(app)
     .get('/')
-    .set('Host', 'test.com')
-    .expect('test.com')
+    .set('Host', 'cat.test.com')
+    .expect('cat.test.com')
+    .end(function (err, res) {
+      if (err) return t.fail(err)
+      t.pass('pass')
+    })
+})
+
+test('custom namespace', function (t) {
+  var app = express()
+  app.use(wildcardSubdomains({
+    domain: 'test.com',
+    namespace: 's',
+    // www: 'false',
+  }))
+
+  app.get('/s/cat', function (req, res) {
+    res.end(req.hostname)
+  })
+
+  t.plan(1)
+  request(app)
+    .get('/')
+    .set('Host', 'cat.test.com')
+    .expect('cat.test.com')
+    .end(function (err, res) {
+      if (err) return t.fail(err)
+      t.pass('pass')
+    })
+})
+
+test('ignore www', function (t) {
+  var app = express()
+  app.use(wildcardSubdomains({
+    domain: 'test.com',
+    namespace: 's',
+    www: true,
+  }))
+
+  app.get('/', function (req, res) {
+    res.end(req.hostname)
+  })
+
+  t.plan(1)
+  request(app)
+    .get('/')
+    .set('Host', 'www.test.com')
+    .expect('www.test.com')
     .end(function (err, res) {
       if (err) return t.fail(err)
       t.pass('pass')
